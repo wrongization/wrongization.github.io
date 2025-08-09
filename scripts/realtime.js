54a9bd94429e48dab7821a5f657e2064e528a8d4
黄韬乐 <23371468@buaa.edu.cn>
2025-08-09 11:38:51 +0800

    Site updated: 2025-08-09 11:38:48

--- scripts/realtime.js
+++ scripts/realtime.js
@@ -1,13 +1,16 @@
 (() => {
-    dayjs.extend(window.dayjs_plugin_duration)
-    const el = document.getElementById('realtime_duration')
-    // 改成自己的时间
-    const date = dayjs('2024-08-04')
-  
-    setInterval(() => {
-      const dur = dayjs.duration(dayjs().diff(date))
-      const days = String(Math.floor(dur.asDays()))
-      const beat = String(Math.floor(dur.asSeconds()))
-      el.innerHTML = '已运行' + days + dur.format('天HH时mm分ss秒')+',是'+beat+'次<i id="heartbeat" class="fa fas fa-heartbeat"></i>'
-    }, 1000)
-  })()
+  dayjs.extend(window.dayjs_plugin_duration);
+  const el = document.getElementById('realtime_duration');
+  const startDate = dayjs('2024-08-04');
+
+  function updateDuration() {
+    const now = dayjs();
+    const duration = dayjs.duration(now.diff(startDate)); 
+    const days = Math.floor(duration.asDays());
+    const seconds = Math.floor(duration.asSeconds());
+    el.innerHTML = `已运行${days}${duration.format('天HH时mm分ss秒')}, 是${seconds}次 <i id="heartbeat" class="fa fas fa-heartbeat"></i>`;
+  }
+
+  updateDuration();
+  setInterval(updateDuration, 1000);
+})();
